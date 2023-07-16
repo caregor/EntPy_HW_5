0c2d09d4abc01798e894270876604643e9706383
caregor <caregor@gmail.com>
2023-07-16 14:17:42 +0500

    Task 2 is done.

--- task2.py
+++ task2.py
@@ -3,3 +3,17 @@
 Напишите функцию, которая принимает на вход строку - абсолютный путь до файла. Функция возвращает
 кортеж из трёх элементов: путь, имя файла, расширение файла.
 """
+
+import os
+
+def split_file_path(file_path):
+    path, filename = os.path.split(file_path)
+    name, extension = os.path.splitext(filename)
+    return path, name, extension
+
+
+file_path = '/path/to/file.txt'
+path, name, extension = split_file_path(file_path)
+print("Путь:", path)
+print("Имя файла:", name)
+print("Расширение файла:", extension)
