2cea441bbde0c231195fca1fe36c8e3f19e11b76
caregor <caregor@gmail.com>
2023-07-16 14:37:37 +0500

    Task 4 is done.

--- task4.py
+++ task4.py
@@ -2,3 +2,17 @@
     ---Task 4---
 Создайте функцию генератор чисел Фибоначчи
 """
+
+
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+fib_gen = fibonacci_generator()
+
+for _ in range(10):
+    fib_num = next(fib_gen)
+    print(fib_num)
