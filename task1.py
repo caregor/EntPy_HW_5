992b64d03871b50de79f4c7ed6353634ebf330fe
caregor <caregor@gmail.com>
2023-07-18 10:26:24 +0500

    Task 1 is done.

--- task1.py
+++ task1.py
@@ -6,5 +6,36 @@
       таблицы умножения.
     * Для вывода результата используйте «принт» без перехода на новую строку.
 """
+table = [f"{j} x {i} = {i*j}" for i in range(2, 10) for j in range(2, 10)]
 
-print('\n'.join(['\t'.join([f'{i}x{j}={i*j}' for j in range(2, 11)]) for i in range(2, 10)]))
+nested_list = [table[i:i+4] for i in range(0, len(table), 4)]
+
+odd_el = []
+
+for i in range(len(nested_list)):
+    if i % 2 == 0:
+        print(nested_list[i])
+    else:
+        odd_el.append(nested_list[i])
+print()
+
+for item in odd_el:
+    print(item)
+
+"""
+table = [f"{j} x {i} = {i*j}" for i in range(2, 10) for j in range(2, 10)]
+
+nested_list = [table[i:i+4] for i in range(0, len(table), 4)]
+
+odd_el = [nested_list[i] for i in range(len(nested_list)) if i % 2 != 0]
+even_el = [nested_list[i] for i in range(len(nested_list)) if i % 2 == 0]
+
+for sublist in even_el:
+    print(sublist)
+
+print()
+
+for sublist in odd_el:
+    print(sublist)
+
+"""
